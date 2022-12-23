337d8d0a0fee2114212605636dbeb10f0fe4fe3c
Bastien CARCAT <bastien-carcat@orange.fr>
2022-12-23 12:39:02 +0100

    Fix peer-deps by downgrading react to v17

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import Auth0ProviderWithHistory from './providers/Auth0ProviderWithHistory'
 import App from './views/screens/App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Auth0ProviderWithHistory>
         <App />
       </Auth0ProviderWithHistory>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
