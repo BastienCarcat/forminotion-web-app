4d0d1968a4b6ae7f0f874f10c0cc5df127a03880
Bastien CARCAT <bastien-carcat@orange.fr>
2022-12-24 16:35:00 +0100

    Change env

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
 import { development } from './env/development'
 import { staging } from './env/staging'
+import { production } from './env/production'
 
 export const config = {
   development: development,
-  staging: staging
-}['development']
+  staging: staging,
+  production: production
+}[process.env.REACT_APP_ENVIRONMENT || 'development']
