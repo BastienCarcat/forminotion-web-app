b1bf0d6605ff2821088202b72bc1b992969c2482
Bastien CARCAT <bastien-carcat@orange.fr>
2023-01-05 21:58:31 +0100

    Change Heroku to Railway

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,5 +1,5 @@
 export const production = {
-  baseUrl: 'https://forminotion-back.herokuapp.com/api/',
+  baseUrl: 'https://forminotion-server.up.railway.app/api/',
   auth0: {
     domain: process.env.REACT_APP_AUTH0_DOMAIN,
     clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
