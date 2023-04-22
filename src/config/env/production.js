deb9f744674791509755f19730dc639a976bc75b
Bastien CARCAT <bastien-carcat@orange.fr>
2023-04-22 17:40:57 +0200

    Redirection + remove home

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,6 +1,6 @@
 export const production = {
   apiUrl: 'https://forminotion-server.up.railway.app/api/',
-  appUrl: 'https://www.forminotion.com/',
+  appUrl: 'https://www.app.forminotion.com/',
   auth0: {
     domain: process.env.REACT_APP_AUTH0_DOMAIN,
     clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
--- src/views/screens/App.js
+++ src/views/screens/App.js
@@ -1,26 +1,22 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import axios from 'axios'
 import React, { useEffect } from 'react'
-import { Route, Routes, useLocation, Navigate } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import { config } from './../../config/index'
 import FormScreen from './Form'
-import _ from 'lodash'
-import HomeScreen from './Home'
 import FormsListScreen from './Forms'
 import FormEditionScreen from './Edition'
 import Loader from '../ui/Globals/Loader'
-import PricingScreen from './Pricing'
 import LogoutScreen from './Logout'
 import FormDetailsScreen from './Details'
 import TermsAndConditions from '../components/Global/Footer/TermsAndConditions'
 import PrivcyLegacy from '../components/Global/Footer/PrivcyLegacy'
 import FormCreationScreen from './Creation'
+import LoginRedirect from '../redirections/login'
 
 const App = () => {
   const { apiUrl } = config || {}
-  const { isAuthenticated, isLoading } = useAuth0()
-
-  const location = useLocation()
+  const { isLoading } = useAuth0()
 
   useEffect(() => {
     axios.defaults.baseURL = apiUrl
@@ -35,31 +31,24 @@ const App = () => {
 
   return (
     <>
-      {_.startsWith(_.get(location, 'pathname'), '/form/') ? (
-        <Routes>
-          <Route path="/form/:idForm" element={<FormScreen />} />
-        </Routes>
-      ) : (
-        <Routes>
-          {isAuthenticated && (
-            <>
-              <Route path="/forms" element={<FormsListScreen />} />
-              <Route path="/creation" element={<FormCreationScreen />} />
-              <Route path="/edition/:idForm" element={<FormEditionScreen />} />
-              <Route path="/details/:idForm" element={<FormDetailsScreen />} />
-            </>
-          )}
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/pricing" element={<PricingScreen />} />
-          <Route path="/logout" element={<LogoutScreen />} />
-          <Route
-            path="/terms-and-conditions"
-            element={<TermsAndConditions />}
-          />
-          <Route path="/privacy-policy" element={<PrivcyLegacy />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      )}
+      <Routes>
+        {/*Authentication required*/}
+        <Route path="/creation" element={<FormCreationScreen />} />
+        <Route path="/edition/:idForm" element={<FormEditionScreen />} />
+        <Route path="/details/:idForm" element={<FormDetailsScreen />} />
+        <Route path="/" element={<FormsListScreen />} />
+
+        {/*Public*/}
+        <Route path="/form/:idForm" element={<FormScreen />} />
+
+        <Route path="/logout" element={<LogoutScreen />} />
+        <Route path="/login-redirect" element={<LoginRedirect />} />
+
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/privacy-policy" element={<PrivcyLegacy />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   )
 }
